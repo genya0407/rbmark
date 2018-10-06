0cfd3fd272afa6146ee836148b437921e1a067de
Yusuke Sangenya <longinus.eva@gmail.com>
2018-10-06 10:47:15 +0900

    capture stdout

--- main.rb
+++ main.rb
@@ -1,15 +1,17 @@
 require 'kramdown'
 require 'erb'
+require 'stringio'
 
 def get_context
   binding
 end
 
 class EvalDoc < Kramdown::Document
-  attr_accessor :context
+  attr_accessor :context, :stdout
 
   def initialize(text, *opts)
     super(text, *opts)
+
     self.context = get_context
     self.root = scan_el(self.root)
   end
@@ -35,30 +37,30 @@ class EvalDoc < Kramdown::Document
   end
 
   def eval_and_elementize(code)
+    stdout = StringIO.new
+    $stdout = stdout
     eval_result = self.context.eval(code)
+    stdout_string = stdout.string.empty? ? '' : stdout.string + "\n"
+    $stdout = STDOUT
 
     case
     when eval_result.respond_to?(:to_html)
       Kramdown::Document.new(eval_result.to_html, input: :html).root
-    when eval_result.respond_to?(:to_s)
-      blockquote_el(eval_result.to_s)
     else
-      blockquote_el(eval_result.inspect)
+      text_result_el(stdout_string + "=> #{eval_result}")
     end
   end
 
-  def blockquote_el(text)
-    text_el = Kramdown::Element.new(:text, text)
+  def text_result_el(text)
+    text_el = Kramdown::Element.new(:codeblock, text)
     p_el = Kramdown::Element.new(:p)
     p_el.children << text_el
-    block_quote_el = Kramdown::Element.new(:blockquote)
-    block_quote_el.children << p_el
-    block_quote_el
+    p_el
   end
 end
 
 def main # bindingを隔離するため
-  doc = EvalDoc.new(STDIN.read)
+  doc = EvalDoc.new(STDIN.read) # ERBの中で使っている
 
   puts ERB.new(File.read('./template.html.erb')).result(binding)
 end
